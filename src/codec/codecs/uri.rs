b3976861b2950b8ad3c593763ae77e0a03d37c1c
brian m. carlson <sandals@crustytoothpaste.net>
2018-10-28 01:46:30 +0000

    codecs: add a uri codec
    Add a codec for URI-encoding.  Currently, in strict mode, the decoder
    rejects all characters that are not produced by the encoder.  This may
    be changed in a future version to be more permissive.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ src/codec/codecs/uri.rs
@@ -0,0 +1,138 @@
+use std::io;
+use codec::Codec;
+use codec::CodecSettings;
+use codec::Direction;
+use codec::Error;
+use codec::FlushState;
+use codec::Status;
+use codec::Transform;
+use codec::StatelessEncoder;
+
+use codec::codecs::hex::{LOWER, UPPER, REV};
+
+pub struct TransformFactory {}
+
+fn forward_transform(inp: &[u8], outp: &mut [u8], arr: &[u8; 16]) -> (usize, usize) {
+    let max = outp.len() - 3;
+    let maxin = inp.len();
+    let (mut i, mut j) = (0, 0);
+    while i < maxin && j < max {
+        let x = inp[i];
+        match x {
+            b'-' | b'.' | b'0'...b'9' | b'A'...b'Z' | b'_' | b'a'...b'z' | b'~' => outp[j] = x,
+            0x00...0x2c | 0x2f | 0x3a...0x40 | 0x5b...0x5e | 0x60 | 0x7b...0x7d | 0x7f...0xff => {
+                outp[j + 0] = b'%';
+                outp[j + 1] = arr[(x as usize) >> 4];
+                outp[j + 2] = arr[(x as usize) & 15];
+                j += 2;
+            }
+            _ => unreachable!(),
+        }
+
+        i += 1;
+        j += 1;
+    }
+    (i, j)
+}
+
+impl TransformFactory {
+    pub fn factory(r: Box<io::BufRead>, s: CodecSettings) -> Result<Box<io::BufRead>, Error> {
+        match s.dir {
+            Direction::Forward => {
+                let arr = match s.args.contains("lower") {
+                    true => &LOWER,
+                    false => &UPPER,
+                };
+                let enc = StatelessEncoder::new(move |inp, out| forward_transform(inp, out, arr));
+                Ok(Box::new(Transform::new(r, enc)))
+            }
+            Direction::Reverse => Ok(Box::new(Transform::new(r, Decoder::new(s.strict)))),
+        }
+    }
+}
+
+pub struct Decoder {
+    strict: bool,
+}
+
+impl Decoder {
+    fn new(strict: bool) -> Self {
+        Decoder { strict: strict }
+    }
+}
+
+impl Codec for Decoder {
+    fn transform(&mut self, src: &[u8], dst: &mut [u8], _f: FlushState) -> Result<Status, Error> {
+        let mut iter = src.iter().enumerate();
+        let mut j = 0;
+        loop {
+            let s = iter.next();
+            let (i, x) = match s {
+                Some((a, b)) => (a, b),
+                None => break,
+            };
+            if j == dst.len() {
+                return Ok(Status::Ok(i, j));
+            }
+
+            match x {
+                b'-' | b'.' | b'0'...b'9' | b'A'...b'Z' | b'_' | b'a'...b'z' | b'~' => dst[j] = *x,
+                b'%' => {
+                    let y = iter.next();
+                    let z = iter.next();
+                    match (y, z) {
+                        (Some((_, a)), Some((_, b))) => {
+                            let v: i16 = ((REV[*a as usize] as i16) << 4) | REV[*b as usize] as i16;
+                            if v < 0 {
+                                return Err(Error::InvalidSequence("uri".to_string(), vec![*a, *b]));
+                            }
+                            dst[j] = v as u8;
+                        }
+                        _ => return Ok(Status::Ok(i - 1, j)),
+                    }
+                }
+                _ => {
+                    if self.strict {
+                        return Err(Error::InvalidSequence("uri".to_string(), vec![*x]));
+                    }
+                    dst[j] = *x;
+                }
+            }
+            j += 1;
+        }
+        return Ok(Status::Ok(src.len(), j));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chain::Chain;
+
+    fn check(inp: &[u8], lower: &[u8], upper: &[u8]) {
+        for i in vec![4, 5, 6, 7, 8, 512] {
+            let c = Chain::new("uri", i, true);
+            assert_eq!(c.transform(inp.to_vec()).unwrap(), upper);
+            let c = Chain::new("uri,lower", i, true);
+            assert_eq!(c.transform(inp.to_vec()).unwrap(), lower);
+            let c = Chain::new("uri,upper", i, true);
+            assert_eq!(c.transform(inp.to_vec()).unwrap(), upper);
+            let c = Chain::new("-uri", i, true);
+            assert_eq!(c.transform(upper.to_vec()).unwrap(), inp);
+            let c = Chain::new("-uri", i, true);
+            assert_eq!(c.transform(lower.to_vec()).unwrap(), inp);
+            let c = Chain::new("-uri", i, false);
+            assert_eq!(c.transform(upper.to_vec()).unwrap(), inp);
+        }
+    }
+
+    #[test]
+    fn encodes_bytes() {
+        check(b"abc", b"abc", b"abc");
+        check(b"\x00\xff", b"%00%ff", b"%00%FF");
+        check(b"\xc2\xa9", b"%c2%a9", b"%C2%A9");
+        check(b"\x01\x23\x45\x67\x89\xab\xcd\xef",
+              b"%01%23Eg%89%ab%cd%ef",
+              b"%01%23Eg%89%AB%CD%EF");
+        check(b"\xfe\xdc\xba", b"%fe%dc%ba", b"%FE%DC%BA");
+    }
+}

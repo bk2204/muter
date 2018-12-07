3ee4702da02924035c12b9beae5e0e76dcfb4f99
brian m. carlson <sandals@crustytoothpaste.net>
2018-12-07 01:17:02 +0000

    codec/registry: allow adding additional codecs
    Allow users to add additional codecs into a CodecRegistry instance.  Add
    tests that the data can be fetched as expected.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- src/codec/registry.rs
+++ src/codec/registry.rs
@@ -23,6 +23,10 @@ impl CodecRegistry {
         CodecRegistry { map: map }
     }
 
+    pub fn insert(&mut self, k: &'static str, f: TransformFactoryFn) {
+        self.map.insert(k, f);
+    }
+
     pub fn create<'a>(&self,
                       name: &'a str,
                       r: Box<io::BufRead>,
@@ -31,6 +35,56 @@ impl CodecRegistry {
         match self.map.get(name) {
             Some(f) => f(r, s),
             None => Err(Error::UnknownCodec(String::from(name))),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use std::io::Read;
+    use std::collections::BTreeSet;
+    use codec::Error;
+    use codec::CodecSettings;
+    use codec::Direction;
+    use codec::registry::CodecRegistry;
+
+    fn factory(_r: Box<io::BufRead>, _s: CodecSettings) -> Result<Box<io::BufRead>, Error> {
+        return Ok(Box::new(io::Cursor::new(vec![0x61, 0x62, 0x63])));
+    }
+
+    fn codec_settings() -> CodecSettings {
+        CodecSettings {
+            args: BTreeSet::new(),
+            bufsize: 512,
+            dir: Direction::Forward,
+            strict: true,
+        }
+    }
+
+    #[test]
+    fn can_fetch_insertable() {
+        let mut cr = CodecRegistry::new();
+        let r = Box::new(io::Cursor::new(Vec::new()));
+
+        match cr.create("random", r, codec_settings()) {
+            Ok(_) => panic!("unexpected success"),
+            Err(Error::UnknownCodec(s)) => assert_eq!(s, "random"),
+            Err(e) => panic!("unexpected error: {:?}", e),
+        }
+
+        cr.insert("random", factory);
+
+        let r = Box::new(io::Cursor::new(Vec::new()));
+        match cr.create("random", r, codec_settings()) {
+            Ok(c) => {
+                let c: Result<Vec<u8>, _> = c.bytes().collect();
+                let v = c.unwrap();
+                assert_eq!(v, vec![0x61, 0x62, 0x63]);
+            }
+            Err(_) => {
+                panic!("failed to insert random");
+            }
         }
     }
 }

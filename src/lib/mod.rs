a9bf3b6dd787318d0bcdd883d7075c7d84522b92
brian m. carlson <sandals@crustytoothpaste.net>
2019-12-01 23:40:05 +0000

    Cargo.toml: use pretty_assertions in tests
    This results in output that is much easier to read when a test fails,
    since it outputs a colored diff of the output.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -5,6 +5,9 @@ extern crate blake2;
 extern crate clap;
 extern crate digest;
 extern crate md5;
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
 extern crate rand_chacha;
 extern crate rand_core;
 extern crate sha1;
